69977ed61db8d62c73cd75c73d7caef548a609f4
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-06-06 12:26:29 +0300

    version 1.0.4
    created a formGroup
    changed ngModel to formControlName
    used Reactive Form instead of Template Driven Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {
     BrowserAnimationsModule,
     NoopAnimationsModule,
 } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
@@ -11,29 +10,30 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-
 import { NgModule } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 import { NgxAgroexToastModule } from '../../projects/ngx-agroex-toast/src/lib/ngx-agroex-toast.module';
-import { FormComponent } from './form/form.component'
 
 @NgModule({
-  declarations: [FormComponent, AppComponent],
+    declarations: [FormComponent, AppComponent],
     imports: [
         BrowserAnimationsModule,
         BrowserModule,
-        FormsModule,
+        MatButtonModule,
         MatButtonToggleModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
         MatSelectModule,
+        NgxAgroexToastModule,
         NoopAnimationsModule,
         OverlayModule,
-        MatButtonModule,
-        NgxAgroexToastModule,
+        ReactiveFormsModule,
     ],
     bootstrap: [AppComponent],
 })
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
-import { AgroexToastService } from '../../../projects/ngx-agroex-toast/src/lib/agroex-toast.service'
-import { IToastOptions } from '../../../projects/ngx-agroex-toast/src/lib/interfaces/toast-options'
-import { ToastType } from '../../../projects/ngx-agroex-toast/src/lib/enums/toast-type'
-
-
+import { AgroexToastService } from '../../../projects/ngx-agroex-toast/src/lib/agroex-toast.service';
+import { ToastType } from '../../../projects/ngx-agroex-toast/src/lib/enums/toast-type';
 
 @Component({
     selector: 'app-form',
@@ -35,18 +33,18 @@ import { ToastType } from '../../../projects/ngx-agroex-toast/src/lib/enums/toas
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormComponent {
-    public toastOption: IToastOptions = {
-        toastType: ToastType.Warning,
-        width: '',
-        title: '',
-        message: '',
-        buttonText: '',
-        timeOut: 5000,
-    };
+    public toastOption = new FormGroup({
+        toastType: new FormControl(ToastType.Error),
+        width: new FormControl(''),
+        title: new FormControl('Error'),
+        message: new FormControl(''),
+        buttonText: new FormControl(''),
+        timeOut: new FormControl(5000),
+    });
 
     constructor(private toastService: AgroexToastService) {}
 
     public returnToastOptions(): void {
-        this.toastService.addToast(this.toastOption);
+        this.toastService.addToast(this.toastOption.value);
     }
 }
